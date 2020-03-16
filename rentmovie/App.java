3458e0df7bb8e11718f0043a1040aa943c3d55bc
hjh0915 <623299157@qq.com>
2020-03-16 16:10:02 +0800

    添加实例

--- rentmovie/App.java
+++ rentmovie/App.java
@@ -4,11 +4,20 @@ import com.work.movie.Rental;
 
 public class App {
     public static void main(String[] args) {
-        Customer customer = new Customer("a");
-        Movie movie = new Movie("The World", 0);
-        Rental rental = new Rental(movie, 7);
-        customer.addRental(rental);
-        String state = customer.statement();
-        System.out.println(state);
+        //第一个客人
+        Customer customer1 = new Customer("a");
+        Movie movie1 = new Movie("The World", 0);
+        Rental rental1 = new Rental(movie1, 7);
+        customer1.addRental(rental1);
+        //第二个客人
+        Customer customer2 = new Customer("b");
+        Movie movie2 = new Movie("A Dog's Life", 1);
+        Rental rental2 = new Rental(movie2, 5);
+        customer2.addRental(rental2);
+
+        String state1 = customer1.statement();
+        String state2 = customer2.statement();
+        System.out.println(state1);
+        System.out.println(state2);
     }
 }
