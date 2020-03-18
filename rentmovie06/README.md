826704f94afaa904893e23296c999ba342c8e20a
hjh0915 <623299157@qq.com>
2020-03-18 20:46:35 +0800

    修改switch代码

--- rentmovie06/README.md
+++ rentmovie06/README.md
@@ -3,3 +3,11 @@ Price类继承
 创建一个Price类，写入一个抽象函数作为父类
 在Movie类中调用方法
 将getCharge()放置在Price类中
+
+修改switch部分代码
+================
+将switch代码中的case分值分别放入相应的类中
+
+重构getFrequentRenterPoints()方法
+================================
+移至Price类中
--- rentmovie06/src/App.java
+++ rentmovie06/src/App.java
@@ -5,7 +5,7 @@ import com.work.movie.Rental;
 public class App {
     public static void main(String[] args) {
         Customer customer = new Customer("a");
-        Movie movie = new Movie("The World", 0);
+        Movie movie = new Movie("The World", 1);
         Rental rental = new Rental(movie, 7);
         customer.addRental(rental);
         String state = customer.statement();
